9f0a2b48cf63ac7f83c53b96d03ca7e029c52287
liliachirwa1116 <lilianchirwa1116@gmail.com>
2023-09-07 11:58:56 -0700

    6-size.c"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main -main function
- * * Return: 0
+ * main - describes the main function of the code
+ * Return: 0
  	*/
 int main(void)
 {
@@ -18,4 +18,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
-
