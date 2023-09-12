bb2236ac277b2fc51d19fcc46667d6012bf72c34
liliachirwa1116 <lilianchirwa1116@gmail.com>
2023-09-12 10:36:43 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the main function
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
